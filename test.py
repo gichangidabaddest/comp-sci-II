1e927e2b4a87c0fa81df6e2f30d2b7bf79fc165b
b-alla <134503700+b-alla@users.noreply.github.com>
2024-10-23 22:07:57 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import qrcode
+
+data = "https://buzzer.lulusms.com/slideshow.php"
+
+# Create a QR code object
+qr = qrcode.QRCode(
+    version=1,  # controls the size of the QR Code
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # controls the error correction used
+    box_size=10,  # controls how many pixels each “box” of the QR code is
+    border=4,  # controls how many boxes thick the border should be
+)
+
+# Add data to the QR code object
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image
+img.save('qrcode.png')
+
+# Optionally, display the image
+img.show()
+print("--"*80 + "finished")
